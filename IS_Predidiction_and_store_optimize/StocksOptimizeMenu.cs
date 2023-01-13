dfbb1e5185642a56ea67107f792b274378773bae
bogdanpavl2000@gmail.com <50374356+LoliScripter@users.noreply.github.com>
2023-01-13 21:57:52 +0300

    Test

---
+++ IS_Predidiction_and_store_optimize/StocksOptimizeMenu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IS_Predidiction_and_store_optimize
+{
+    public partial class StocksOptimizeMenu : Form
+    {
+        public StocksOptimizeMenu()
+        {
+            InitializeComponent();
+        }
+    }
+}
